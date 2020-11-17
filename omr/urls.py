012358ac688787e4a68e6415512c158860721810
Vishnu0179 <vjvj140@gmail.com>
2020-11-18 01:36:41 +0530

    fixed error in urls.py

--- omr/urls.py
+++ omr/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
     path('upload/', views.upload_answer_key, name="answerOmr"),
     path('eval/', views.evaluateAnswerSheet, name="studentOmr"),
     path('students/', views.getSetDetails, name="studentList")
